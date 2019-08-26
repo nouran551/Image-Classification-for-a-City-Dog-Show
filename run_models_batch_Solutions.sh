b85a2d5a6e80027ec78cb14c66af57432e04e8f6
Nouran Waleed <40387105+nouran551@users.noreply.github.com>
2019-08-26 20:54:18 +0200

    Create run_models_batch_Solutions.sh

---
+++ run_models_batch_Solutions.sh
@@ -0,0 +1,3 @@
+python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet.txt
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet.txt
+python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg.txt
